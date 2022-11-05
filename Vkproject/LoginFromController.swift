e1912a471b5dfa3533703a480a57064a4c44f688
Marina <IOSdev.mars@gmail.com>
2022-11-05 18:54:53 +0300

    loginView create

---
+++ Vkproject/LoginFromController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginFromController.swift
+//  Vkproject
+//
+//  Created by Марина Мырксина on 05.11.2022.
+//
+
+import UIKit
+
+class LoginFromController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
